867e78148af110c482a3f9f2a2e7b33db62b1c48
cptnapalm <cptjustice@gmail.com>
2018-03-13 12:40:16 -0500

    Found the 2.11BSD calls for the listed syscalls.

--- libgloss/pdp11/syscalls.c
+++ libgloss/pdp11/syscalls.c
@@ -7,23 +7,23 @@
 #include <sys/time.h>
 #include <stdio.h>
  
-void _exit();
-int close(int file);
+void _exit();                   /* 2.11BSD /usr/src/lib/libc/pdp/sys/_exit.s */
+int close(int file);            /* 2.11BSD /usr/src/sys/kern_descrip.c */
 char **environ; /* pointer to array of char * strings that define the current environment variables */
-int execve(char *name, char **argv, char **env);
-int fork();
-int fstat(int file, struct stat *st);
-int getpid();
-int isatty(int file);
-int kill(int pid, int sig);
-int link(char *old, char *new);
-int lseek(int file, int ptr, int dir);
-int open(const char *name, int flags, ...);
-int read(int file, char *ptr, int len);
-caddr_t sbrk(int incr);
-int stat(const char *file, struct stat *st);
-clock_t times(struct tms *buf);
-int unlink(char *name);
-int wait(int *status);
-int write(int file, char *ptr, int len);
-int gettimeofday(struct timeval *p, struct timezone *z);
+int execve(char *name, char **argv, char **env);    /* 2.11BSD /usr/src/sys/kern_exec.c */
+int fork();                     /* 2.11BSD /usr/src/lib/libc/pdp/sys/fork.s */
+int fstat(int file, struct stat *st);   /* 2.11BSD /usr/src/sys/kern_descrip.c */
+int getpid();                   /* 2.11BSD /usr/src/sys/kern_prot.c */
+int isatty(int file);           /* 2.11BSD /usr/src/lib/libc/gen/isatty.c */
+int kill(int pid, int sig);     /* 2.11BSD /usr/src/sys/ker_sig.c */
+int link(char *old, char *new); /* 2.11BSD /usr/src/sys/ufs_syscalls.c */
+int lseek(int file, int ptr, int dir);  /* 2.11BSD /usr/src/sys/ufs_syscalls2.c */
+int open(const char *name, int flags, ...); /* 2.11BSD /usr/src/sys/ufs_syscalls.c */
+int read(int file, char *ptr, int len); /* 2.11BSD /usr/src/sys/sys_generic.c */
+caddr_t sbrk(int incr);         /* 2.11BSD /usr/src/lib/libc/pdp/sys/sbrk.s */
+int stat(const char *file, struct stat *st);    /* 2.11BSD /usr/src/sys/ufs_syscalls.c */
+clock_t times(struct tms *buf); /* 2.11BSD /usr/src/lib/libc/compat-4.1/times.c */
+int unlink(char *name);         /* 2.11BSD /usr/src/sys/ufs_syscalls.c */
+int wait(int *status);          /* 2.11BSD /usr/src/lib/libc/gen/wait.c */
+int write(int file, char *ptr, int len);    /* 2.11BSD /usr/src/sys/sys_generic.c */
+int gettimeofday(struct timeval *p, struct timezone *z);    /* 2.11BSD /usr/src/sys/kern_time.c */
