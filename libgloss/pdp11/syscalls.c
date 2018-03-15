f3527346443aaeb2009844364458276a3f8d6dd5
cptnapalm <cptjustice@gmail.com>
2018-03-15 16:37:16 -0500

    Compiles.  Compiling, though, doesn't work.

--- libgloss/pdp11/syscalls.c
+++ libgloss/pdp11/syscalls.c
@@ -9,17 +9,17 @@
 
 void _exit();                   /* 2.11BSD /usr/src/lib/libc/pdp/sys/_exit.s */
 int system(const char *command);     /* 2.11BSD /usr/src/lib/libc/gen/system.c */
-//char* malloc(unsigned nbytes);  /* 2.11BSD /usr/src/lib/libc/gen/malloc.c */
-//char* getenv(char *name);       /* ? */
+char* malloc(unsigned nbytes);  /* 2.11BSD /usr/src/lib/libc/gen/malloc.c */
+char* getenv(char *name);       /* ? */
 int execve(char *name, char **argv, char **env);    /* 2.11BSD /usr/src/sys/sys/kern_exec.c */
 int fork();                     /* 2.11BSD /usr/src/lib/libc/pdp/sys/fork.s */
-//caddr_t sbrk(int incr);         /* 2.11BSD /usr/src/lib/libc/pdp/sys/sbrk.s */
+caddr_t sbrk(int incr);         /* 2.11BSD /usr/src/lib/libc/pdp/sys/sbrk.s */
 int getpid();                   /* 2.11BSD /usr/src/sys/sys/kern_prot.c ? */
-//char **environ; /* pointer to array of char * strings that define the current environment variables */
+char **environ; /* pointer to array of char * strings that define the current environment variables */
 int fstat(int file, struct stat *st);   /* 2.11BSD /usr/src/sys/sys/kern_descrip.c ? */
 int isatty(int file);           /* 2.11BSD /usr/src/lib/libc/gen/isatty.c */
 int kill(int pid, int sig);     /* 2.11BSD /usr/src/sys/sys/ker_sig.c */
-//clock_t times(struct tms *buf); /* 2.11BSD /usr/src/lib/libc/compat-4.1/times.c */
+clock_t times(struct tms *buf); /* 2.11BSD /usr/src/lib/libc/compat-4.1/times.c */
 int gettimeofday(struct timeval *p, struct timezone *z);    /* 2.11BSD /usr/src/sys/kern_time.c */
 int wait(int *status);          /* 2.11BSD /usr/src/lib/libc/gen/wait.c */
 int stat(const char *file, struct stat *st);    /* 2.11BSD /usr/src/sys/ufs_syscalls.c */
